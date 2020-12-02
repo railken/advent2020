3c1485631047299c58ce6818de23d40e0c5ccb7f
railken <railken@protonmail.ch>
2020-12-02 18:38:21 +0100

    refactor tests

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -3,6 +3,9 @@
 namespace Railken\Advent2020\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Process\Process;
 
 abstract class BaseTest extends TestCase
 {
@@ -22,6 +25,44 @@ abstract class BaseTest extends TestCase
         }
 
         mkdir($dir, 0777, true);
+    } 
+    
+    public function commonDay($path, $file)
+    {
+
+        $application = new Application();
+
+        $application->add(new \Railken\Advent2020\Day1\PuzzleACommand());
+        $application->add(new \Railken\Advent2020\Day1\PuzzleBCommand());
+
+        $command = $application->find($path);
+        $commandTester = new CommandTester($command);
+
+        $commandTester->setInputs([
+        
+        ]);
+
+        $diff = 0;
+        $total = 10;
+
+        for ($i = 0; $i < $total; $i++) {
+
+            $start = microtime(true);
+
+            $commandTester->execute([
+                'command' => $command->getName(),
+                'path' => $file
+            ]);
+
+            $end = microtime(true);
+
+            $diff += $end - $start;
+        }
+
+
+        print_r("\n\n<Name:'$path'> <AVG:".number_format($diff/$total, 8)."s> <Total:".$total.">\n");
+
+        return $commandTester->getDisplay();
     }
 
     public function getDir()
--- tests/Day1/Day1CommandTest.php
+++ tests/Day1/Day1CommandTest.php
@@ -2,47 +2,17 @@
 
 namespace Railken\Advent2020\Tests;
 
-use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Tester\CommandTester;
-use Symfony\Component\Process\Process;
 use Railken\Advent2020\Tests\BaseTest;
 
 class Day1CommandTest extends BaseTest
 {
-    public function commonDay($path) {
-
-        $start = microtime(true);
-        print_r("\n\n$path: ");
-        $application = new Application();
-
-        $application->add(new \Railken\Advent2020\Day1\PuzzleACommand());
-        $application->add(new \Railken\Advent2020\Day1\PuzzleBCommand());
-
-        $command = $application->find($path);
-        $commandTester = new CommandTester($command);
-
-        $commandTester->setInputs([
-        ]);
-
-        $commandTester->execute([
-            'command' => $command->getName(),
-            'path' => __DIR__."/input.txt"
-        ]);
-
-        $end = microtime(true);
-
-        print_r($end - $start ."s\n");
-
-        return $commandTester->getDisplay();
-    }
-
     public function testDay1PuzzleA()
     {
-        $this->assertEquals("1007331", $this->commonDay('day1:puzzleA')); // ~ 0.009 sec
+        $this->assertEquals("1007331", $this->commonDay('day1:puzzleA', __DIR__."/input.txt")); // ~ 0.00202346s
     }
 
     public function testDay1PuzzleB()
     {
-        $this->assertEquals("48914340", $this->commonDay('day1:puzzleB')); // ~0.011 sec
+        $this->assertEquals("48914340", $this->commonDay('day1:puzzleB', __DIR__."/input.txt")); // ~0.01146319s
     }
 }
